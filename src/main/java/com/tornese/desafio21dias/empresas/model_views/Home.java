553cdf8e81f69537f753c511c82db9d0ab818a08
EdHolanda <edilson_holanda@hotmail.com>
2021-12-08 21:08:42 -0300

    Um pouco sobre Rest em Java

---
+++ src/main/java/com/tornese/desafio21dias/empresas/model_views/Home.java
@@ -0,0 +1,7 @@
+package com.tornese.desafio21dias.empresas.model_views;
+
+public class Home {
+    public String getMensagem(){
+        return "Bem vindo à API em Java, feita no desafio 21 dias múltiplas APIs";
+    }    
+}
